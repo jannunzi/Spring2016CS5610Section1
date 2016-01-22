c3b3902495986583b48c17e68b58e946185a09ed
JoseAnnunziatoNeu <jannunzi@gmail.com>
2016-01-22 10:47:00 -0500

    moved index.html simplified server.js added dashboard experiment

--- server.js
+++ server.js
@@ -6,18 +6,4 @@ app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-app.get('/say/hello', sayHello);
-app.get('/api/users', function(req, res){
-    var users = [
-        {username: 'alice', first: 'Alice', last: "Wonderland"},
-        {username: 'bob', first: 'Bob', last: "Hope"},
-        {username: 'charlie', first: 'Charlie', last: "Garcia"}
-    ];
-    res.json(users);
-});
-
-function sayHello(req, res){
-    res.send('hello world 123');
-}
-
 app.listen(port, ipaddress);
